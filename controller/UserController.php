ac11573f905e7ad88617490a12c94c972d186d17
Eskibal <jeanpatrick.esquibal@gmail.com>
2025-03-20 11:30:44 +0100

    commit

--- controller/UserController.php
+++ controller/UserController.php
@@ -15,26 +15,30 @@
 
             $this->conn = new mysqli($servername, $username, $password, $dbname);
             
-            // Check connecti
+            // Check connection
             if ($this->conn->connect_error) {
                 die("Connection failed: " . $this->conn->connect_error);
             }
             echo "Connection Succesfully";
         }
         public function login(): void {
+            // get data from form request
             $username = $_POST["username"];
             $password = $_POST["password"];
 
+            //check BBDD
             $stmt = $this->conn->prepare(query: "SELECT name, password FROM users WHERE name=? AND password=?");
             $stmt->bind_param( "ss", $username, $password);
             $stmt->execute();
         
             if ($stmt->fetch()) {
+                // authentication success
                 $_SESSION["logged"] = true;
                 $_SESSION["user"] = $username;
-
+                // close connection
                 $this->conn->close();
 
+                // redirect to home page
                 header(header: "Location: ../view/profile.php");
                 exit();
             }
