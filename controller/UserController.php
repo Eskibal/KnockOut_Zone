14985f8ed9af212b323e70a24c3e7e502b09d540
Eskibal <jeanpatrick.esquibal@gmail.com>
2025-03-20 11:27:17 +0100

    Co-authored-by: Victor Liu <LiuUexe@users.noreply.github.com>

--- controller/UserController.php
+++ controller/UserController.php
@@ -8,20 +8,30 @@
         private $conn;
 
         public function __construct() {
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "knockoutzone";
+
+            $this->conn = new mysqli($servername, $username, $password, $dbname);
             
+            // Check connecti
+            if ($this->conn->connect_error) {
+                die("Connection failed: " . $this->conn->connect_error);
+            }
+            echo "Connection Succesfully";
         }
-
         public function login(): void {
-            $username = $_POST['username'];
-            $password = $_POST['password'];
+            $username = $_POST["username"];
+            $password = $_POST["password"];
 
-            $stmt = $this->conn->prepare(query: "SELECT Username, Passwd FROM Users WHERE Username=? AND Passwd=?");
-            $stmt->bind_param(types: "ss", var: $username, vars: $password);
+            $stmt = $this->conn->prepare(query: "SELECT name, password FROM users WHERE name=? AND password=?");
+            $stmt->bind_param( "ss", $username, $password);
             $stmt->execute();
         
             if ($stmt->fetch()) {
-                $_SESSION['logged'] = true;
-                $_SESSION['user'] = $username;
+                $_SESSION["logged"] = true;
+                $_SESSION["user"] = $username;
 
                 $this->conn->close();
 
