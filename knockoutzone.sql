c03853dda20cd34548259077836d201d00f0c4ad
Eskibal <jeanpatrick.esquibal@gmail.com>
2025-04-03 09:29:33 +0200

    commit

---
+++ knockoutzone.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE Knockoutzone;
+USE Knockoutzone;
+
+CREATE TABLE Users (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) UNIQUE NOT NULL, -- Username
+    email VARCHAR(75) UNIQUE NOT NULL,
+    password VARCHAR(90) UNIQUE NOT NULL -- User Password
+);
