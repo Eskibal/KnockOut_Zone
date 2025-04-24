19357adcdc42ae1e0e789efeab1fc8458f89be15
Eskibal <jeanpatrick.esquibal@gmail.com>
2025-04-24 10:29:38 +0200

    changed "profile_img" to "path_pfp"

--- view/profile.php
+++ view/profile.php
@@ -3,14 +3,14 @@ session_start();
 $conn = new mysqli("localhost", "root", "", "knockoutzone");
 $user = $_SESSION["user"];
 
-$query = $conn->prepare("SELECT profile_img FROM users WHERE name = ?");
+$query = $conn->prepare("SELECT path_pfp FROM users WHERE name = ?");
 $query->bind_param("s", $user);
 $query->execute();
 $result = $query->get_result();
 $row = $result->fetch_assoc();
 
-if (!empty($row["profile_img"])) {
-    echo "<img src='../images/profiles/" . $row["profile_img"] . "' alt='Perfil' style='width:150px; border-radius:50%;'>";
+if (!empty($row["path_pfp"])) {
+    echo "<img src='../images/profiles/" . $row["path_pfp"] . "' alt='Perfil' style='width:150px; border-radius:50%;'>";
 } else {
     echo "<p>No profile picture</p>";
 }
